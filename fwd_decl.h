afaa128b5f39cbdfbcf22ec6c280f0617e1fda28
Kirito <1362050620@qq.com>
2016-11-21 11:45:37 +0800

    Create fwd_decl.h

---
+++ fwd_decl.h
@@ -0,0 +1,9 @@
+/** Forward Declaration */
+
+int _SortScoreInfoByScoreA(const void*,const void*);
+int _init_cache();
+int _Do_Load(int);
+void _Load_Settings();
+int _Do_Save(int);
+void _Notify_Cache_Class_OutOfDate();
+void _Notify_Cache_Class_OutOfDate_Single(StudentInfo*);
